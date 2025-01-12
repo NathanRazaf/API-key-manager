6e255530b59942c5a00b23f8dc1166ac5635e388
NathanRazaf <natrazaf2020@gmail.com>
2025-01-11 23:54:02 -0500

    Initial commit

---
+++ src/test/java/org/asp3rity/apikeymanager/ApiKeyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asp3rity.apikeymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKeyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
